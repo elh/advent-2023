9e7716316a03625b85288d0b55322c4a710b1440
eugene huang <eugene@goforward.com>
2023-12-02 15:26:57 +0800

    add day 2 p1 and p2

---
+++ 2.py
@@ -0,0 +1,57 @@
+import argparse
+
+def parse_game(line: str) -> dict:
+    parts = line.split(": ")
+    out = {
+        "number": int(parts[0].split(" ")[1]),
+        "rounds": [],
+    }
+
+    for round in parts[1].split("; "):
+        r = {}
+        for color_t in round.split(", "):
+            color_parts = color_t.split(" ")
+            r[color_parts[1]] = int(color_parts[0])
+        out["rounds"].append(r)
+
+    return out
+
+def p1(input: str) -> bool:
+    cubes = {"red": 12, "green": 13, "blue": 14}
+    total = 0
+    for line in input.split("\n"):
+        game = parse_game(line)
+        valid = True
+        for round in game["rounds"]:
+            for color in round:
+                if round[color] > cubes[color]:
+                    valid = False
+                    break
+        if valid:
+            total += game["number"]
+    return total
+
+def p2(input: str) -> bool:
+    total = 0
+    for line in input.split("\n"):
+        game = parse_game(line)
+        cubes = {"red": 0, "green": 0, "blue": 0}
+        for round in game["rounds"]:
+            for color in round:
+                if round[color] > cubes[color]:
+                    cubes[color] = round[color]
+        total += cubes["red"] * cubes["green"] * cubes["blue"]
+    return total
+
+def main(input: str) -> int:
+    # return p1(input)
+    return p2(input)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    args = parser.parse_args()
+
+    f = open(args.input_file, "r")
+    input = f.read().rstrip("\n")
+    print(main(input))
