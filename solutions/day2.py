77bef45bdc7c9e87e64612064e1e40f2a83b168a
eugene huang <eugene@goforward.com>
2023-12-03 07:15:24 +0800

    list comprehension is more pythonic

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,4 +1,4 @@
-def parse_game(line: str) -> dict:
+def parse_game(line: str) -> int:
     parts = line.split(": ")
     out = {
         "number": int(parts[0].split(" ")[1]),
@@ -15,8 +15,8 @@ def parse_game(line: str) -> dict:
     return out
 
 
-def part1(input: str) -> bool:
-    cubes = {"red": 12, "green": 13, "blue": 14}
+def part1(input: str) -> int:
+    cubes: dict[str, int] = {"red": 12, "green": 13, "blue": 14}
 
     def points(game: dict) -> int:
         if all(
@@ -25,15 +25,15 @@ def part1(input: str) -> bool:
             return game["number"]
         return 0
 
-    return sum(map(lambda line: points(parse_game(line)), input.split("\n")))
+    return sum([points(parse_game(line)) for line in input.split("\n")])
 
 
 def part2(input: str) -> bool:
     def points(game: dict) -> int:
-        cubes = {"red": 0, "green": 0, "blue": 0}
+        cubes: dict[str, int] = {"red": 0, "green": 0, "blue": 0}
         for round in game["rounds"]:
             for color in round:
                 cubes[color] = max(cubes[color], round[color])
         return cubes["red"] * cubes["green"] * cubes["blue"]
 
-    return sum(map(lambda line: points(parse_game(line)), input.split("\n")))
+    return sum([points(parse_game(line)) for line in input.split("\n")])
