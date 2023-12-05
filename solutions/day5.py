4339feb188b4900e12c60dd3c6193f4d5d1781fa
eugene huang <eugene@goforward.com>
2023-12-04 23:46:24 -0800

    add day 5 p2

--- solutions/day5.py
+++ solutions/day5.py
@@ -13,6 +13,27 @@ def parse_input(input: str) -> dict:
     }
 
 
+def get_reversed_value(v: int, data: dict) -> int:
+    for m in reversed(data["maps"]):
+        for dest_start, source_start, range_len in m:
+            if v >= dest_start and v < dest_start + range_len:
+                v = source_start + (v - dest_start)
+                break
+    return v
+
+
+def is_valid_init_value(v: int, data: dict) -> bool:
+    i = 0
+    while i < len(data["initial_values"]):
+        if (
+            v >= data["initial_values"][i]
+            and v < data["initial_values"][i] + data["initial_values"][i + 1]
+        ):
+            return True
+        i += 2
+    return False
+
+
 def part1(input: str) -> int:
     parsed = parse_input(input)
 
@@ -28,5 +49,20 @@ def part1(input: str) -> int:
     return min(vs)
 
 
+# brute force by reversing them through the maps starting from 0 until we find
+# a valid initial value
 def part2(input: str) -> int:
-    return 0
+    parsed = parse_input(input)
+
+    n = 0
+    while True:
+        v = get_reversed_value(n, parsed)
+        if is_valid_init_value(v, parsed):
+            return n
+
+        # next
+        n += 1
+        # if n % 100000 == 0:
+        #     print("n =", n)
+        if n > 1000000000:
+            raise Exception("too many iterations")
