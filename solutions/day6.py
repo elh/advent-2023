ee3f29ef6448134c2dae94bb17d63b259f28c121
eugene huang <eugene@goforward.com>
2023-12-05 23:00:22 -0800

    remove buggy binary search (off by one). solve w/ math

--- solutions/day6.py
+++ solutions/day6.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import operator
+from math import sqrt, floor, ceil
 
 
 # a list of races where each race is a tuple (time, distance)
@@ -8,25 +9,24 @@ def parse_input(input: str) -> list:
     return list(zip(list(map(int, lists[0][1:])), list(map(int, lists[1][1:]))))
 
 
-def distance(time_held: int, total_time: int) -> int:
-    if time_held >= total_time:
-        return 0
-    return time_held * (total_time - time_held)
-
-
 def product(l: list) -> int:
     return reduce(operator.mul, l, 1)
 
 
+# return # of ways to hold to exceed dist
+def number_of_ways(time: int, dist: int) -> int:
+    """
+    distance = time * (time - time_held)
+    time_held = (time ± sqrt(time**2 - 4 * distance)) / 2
+    """
+    th1 = floor((time + sqrt(time**2 - 4 * dist)) / 2)
+    th2 = ceil((time - sqrt(time**2 - 4 * dist)) / 2)
+    return th1 - th2 + 1
+
+
 def part1(input: str) -> int:
     races = parse_input(input)
-
-    all_ways = []
-    for race in races:
-        ways = sum(
-            [1 if distance(i, race[0]) > race[1] else 0 for i in range(0, race[0])]
-        )
-        all_ways.append(ways)
+    all_ways = [number_of_ways(race[0], race[1]) for race in races]
     return product(all_ways)
 
 
@@ -38,28 +38,4 @@ def part2(input: str) -> int:
     time = combined_int([r[0] for r in races])
     dist = combined_int([r[1] for r in races])
 
-    # binary search for the smallest the time between 0 and time that will give us the distance(t) > dist
-    l = 0
-    r = time
-    while l < r:
-        m = (l + r) // 2
-        if distance(m, time) > dist:
-            r = m
-        else:
-            l = m + 1
-    lowest = l
-
-    # binary search for the largest the time between 0 and time that will give us the distance(t) > dist
-    l = 0
-    r = time
-    while l < r:
-        m = (l + r) // 2
-        if distance(m, time) > dist:
-            l = m + 1
-        else:
-            r = m
-    highest = r
-
-    print(lowest, highest)
-
-    return highest - lowest
+    return number_of_ways(time, dist)
