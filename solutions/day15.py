0cf105cd6e3bfd929565007b290af3f5f57e9f91
eugene huang <eugene@goforward.com>
2023-12-17 08:16:17 +0530

    add day 15 p2

--- solutions/day15.py
+++ solutions/day15.py
@@ -1,5 +1,25 @@
-def parse_input(input: str) -> list[str]:
-    return input.split(",")
+def parse_input(input: str) -> list[dict[str, str]]:
+    out = []
+    for step in input.split(","):
+        if step.endswith("-"):
+            out.append(
+                {
+                    "raw": step,
+                    "op": "-",
+                    "label": step[:-1],
+                }
+            )
+        else:
+            parts = step.split("=")
+            out.append(
+                {
+                    "raw": step,
+                    "op": "=",
+                    "label": parts[0],
+                    "value": parts[1],
+                }
+            )
+    return out
 
 
 def holiday_hash(s: str) -> int:
@@ -11,15 +31,52 @@ def holiday_hash(s: str) -> int:
     return cur
 
 
+class HolidayHashMap:
+    def __init__(self) -> None:
+        self.dict: dict[int, list[tuple[str, int]]] = {}
+
+    def add(self, k: str, v: int) -> None:
+        h = holiday_hash(k)
+        if h not in self.dict:
+            self.dict[h] = []
+
+        key_exists = k in [t[0] for t in self.dict[h]]
+        if key_exists:
+            self.dict[h] = [(k, v) if t[0] == k else t for t in self.dict[h]]
+        else:
+            self.dict[h].append((k, v))
+
+    def remove(self, k: str) -> None:
+        h = holiday_hash(k)
+        if h in self.dict:
+            self.dict[h] = [t for t in self.dict[h] if t[0] != k]
+
+
+def focusing_power(lens_boxes: dict[int, list[tuple[str, int]]]) -> int:
+    total = 0
+    for box_num, lenses in lens_boxes.items():
+        for i, lens in enumerate(lenses):
+            total += (box_num + 1) * (i + 1) * lens[1]
+
+    return total
+
+
 def part1(input: str) -> int:
     steps = parse_input(input)
-    values = [holiday_hash(step) for step in steps]
+    values = [holiday_hash(step["raw"]) for step in steps]
     return sum(values)
 
 
 def part2(input: str) -> int:
-    data = parse_input(input)
-    _ = data
-    # print(data)
+    steps = parse_input(input)
 
-    return 0
+    hm = HolidayHashMap()
+    for step in steps:
+        if step["op"] == "=":
+            hm.add(step["label"], int(step["value"]))
+        elif step["op"] == "-":
+            hm.remove(step["label"])
+        else:
+            raise Exception("unsupported op")
+
+    return focusing_power(hm.dict)
