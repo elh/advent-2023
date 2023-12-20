bd697da3007b74551cb31e256297383689208e64
eugene huang <eugene@goforward.com>
2023-12-19 22:16:13 -0800

    clean up day 20 p1

--- solutions/day20.py
+++ solutions/day20.py
@@ -1,9 +1,12 @@
+import pprint
+
 ITERATIONS = 1000
+VERBOSE = False
 
 
 def parse_modules(input: str):
     lines = [line for line in input.split("\n") if not line.startswith("#")]
-    modules = {}
+    modules: dict[str, dict] = {}
     for line in lines:
         parts = line.split(" -> ")
 
@@ -39,6 +42,11 @@ def parse_modules(input: str):
     return modules
 
 
+def debug(args):
+    if VERBOSE:
+        pprint.pprint(args)
+
+
 def flip(state: str) -> str:
     if state == "on":
         return "off"
@@ -46,44 +54,58 @@ def flip(state: str) -> str:
         return "on"
 
 
+Pulse = tuple[str, str, str]  # (from, "high"/"low", to)
+
+
+# mutates the state of modules
+def press_button(modules: dict) -> list[Pulse]:
+    pulse_history = []
+
+    pulses = [("button", "low", "broadcaster")]
+    while len(pulses) > 0:
+        pulse = pulses.pop(0)
+        pulse_history.append(pulse)
+        pulse_from, pulse_highlow, pulse_to = pulse
+        debug(modules)
+        debug((pulse_from, pulse_highlow, pulse_to))
+
+        if pulse_to not in modules:
+            continue
+
+        pulse_to_module = modules[pulse_to]
+        if pulse_to_module["type"] == "flip":
+            if pulse_highlow == "high":
+                continue
+            pulse_to_module["state"] = flip(pulse_to_module["state"])
+            new_highlow = "high" if pulse_to_module["state"] == "on" else "low"
+
+            for dest in pulse_to_module["dests"]:
+                pulses.append((pulse_to, new_highlow, dest))
+        elif pulse_to_module["type"] == "conj":
+            pulse_to_module["remembered"][pulse_from] = pulse_highlow
+            if "low" in pulse_to_module["remembered"].values():
+                new_highlow = "high"
+            else:
+                new_highlow = "low"
+            for dest in pulse_to_module["dests"]:
+                pulses.append((pulse_to, new_highlow, dest))
+        elif pulse_to_module["type"] == "broadcaster":
+            for dest in pulse_to_module["dests"]:
+                pulses.append((pulse_to, pulse_highlow, dest))
+        else:
+            raise Exception("bad module type")
+
+    return pulse_history
+
+
 def part1(input: str) -> int:
     modules = parse_modules(input)
 
     low_pulse_count, high_pulse_count = 0, 0
     for _ in range(ITERATIONS):
-        pulses = [("button", "low", "broadcaster")]  # tuple of (from, high/low, to)
-        while len(pulses) > 0:
-            pulse_from, pulse_highlow, pulse_to = pulses.pop(0)
-            if pulse_highlow == "low":
-                low_pulse_count += 1
-            else:
-                high_pulse_count += 1
-
-            if pulse_to not in modules:
-                continue
-
-            pulse_to_module = modules[pulse_to]
-            if pulse_to_module["type"] == "flip":
-                if pulse_highlow == "high":
-                    continue
-                pulse_to_module["state"] = flip(pulse_to_module["state"])
-                new_highlow = "high" if pulse_to_module["state"] == "on" else "low"
-
-                for dest in pulse_to_module["dests"]:
-                    pulses.append((pulse_to, new_highlow, dest))
-            elif pulse_to_module["type"] == "conj":
-                pulse_to_module["remembered"][pulse_from] = pulse_highlow
-                if "low" in pulse_to_module["remembered"].values():
-                    new_highlow = "high"
-                else:
-                    new_highlow = "low"
-                for dest in pulse_to_module["dests"]:
-                    pulses.append((pulse_to, new_highlow, dest))
-            elif pulse_to_module["type"] == "broadcaster":
-                for dest in pulse_to_module["dests"]:
-                    pulses.append((pulse_to, pulse_highlow, dest))
-            else:
-                raise Exception("bad module type")
+        pulses = press_button(modules)
+        low_pulse_count += len([pulse for pulse in pulses if pulse[1] == "low"])
+        high_pulse_count += len([pulse for pulse in pulses if pulse[1] == "high"])
 
     return low_pulse_count * high_pulse_count
 
