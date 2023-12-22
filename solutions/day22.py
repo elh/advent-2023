fc19ef8dae8040c5b30b4fed4395d088346cf262
eugene huang <eugene@goforward.com>
2023-12-21 23:19:32 -0800

    add day 22 p2

--- solutions/day22.py
+++ solutions/day22.py
@@ -1,12 +1,4 @@
-import pprint
 import copy
-
-VERBOSE = False
-
-
-def debug(*args):
-    if VERBOSE:
-        print(args)
 
 
 # return a list of tuples (start, end) where start and end are a pair of x,y,z
@@ -20,9 +12,12 @@ def parse_input(input: str) -> list[list[list[int]]]:
         for part_str in parts_str:
             parts.append([int(v) for v in (part_str.split(","))])
         bricks.append(parts)
+    # sorted by z value. they fall in this order
+    bricks.sort(key=lambda b: min(b[0][2], b[1][2]))
     return bricks
 
 
+# lol: still remember first deriving this working on appt scheduling
 def overlaps(a_start, a_stop, b_start, b_stop) -> bool:
     return a_start <= b_stop and b_start <= a_stop
 
@@ -30,25 +25,21 @@ def overlaps(a_start, a_stop, b_start, b_stop) -> bool:
 # return true if the brick is blocked on the z axis going down by any of the
 # other blocks
 def is_blocked(
-    brick: tuple[tuple[int, int, int], tuple[int, int, int]],
-    other_bricks: list[tuple[tuple[int, int, int], tuple[int, int, int]]],
-) -> bool:
+    brick: list[list[int]],
+    other_bricks: list[list[list[int]]],
+) -> tuple[bool, list[list[list[int]]]]:
     brick_lowest_z = min(brick[0][2], brick[1][2])
 
-    debug("is_blocked", brick, other_bricks)
     is_blocked = False
     blockers = []
     for other_brick in other_bricks:
-        debug("checking blocked", brick, other_brick)
         other_highest_z = max(other_brick[0][2], other_brick[1][2])
 
-        debug("other_highest_z", other_highest_z)
-        debug("brick_lowest_z", brick_lowest_z)
-
-        is_right_above = other_highest_z + 1 == brick_lowest_z
-
-        if not is_right_above:
+        if not other_highest_z + 1 == brick_lowest_z:
+            # block has to be right above you
             continue
+        # TODO: perf: maybe worth sorting the bricks by z so we can break early on gaps
+        #       could do it by keeping bricks as a dict keyed by z
 
         x_overlaps = overlaps(
             brick[0][0], brick[1][0], other_brick[0][0], other_brick[1][0]
@@ -56,45 +47,41 @@ def is_blocked(
         y_overlaps = overlaps(
             brick[0][1], brick[1][1], other_brick[0][1], other_brick[1][1]
         )
-
-        debug("x_overlaps", x_overlaps)
-        debug("y_overlaps", y_overlaps)
-        debug("is_right_above", is_right_above)
-
         if x_overlaps and y_overlaps:
-            debug("BLOCKED")
             is_blocked = True
             blockers.append(other_brick)
 
     return is_blocked, blockers
 
 
-def drop(bricks: list[list[list[int]]]) -> list[list[list[int]]]:
-    blocking_bricks = {}  # brick -> bricks it depends on
+def drop(
+    bricks: list[list[list[int]]],
+) -> tuple[list[list[list[int]]], dict[tuple, set]]:
+    blocking_bricks: dict[tuple, set] = {}  # brick -> bricks it depends on
 
     bricks = copy.deepcopy(bricks)
     for i in range(len(bricks)):
         brick = bricks[i]
         previous_bricks = bricks[:i]
-        debug("dropping", brick)
         while True:
             # drop the brick until it hits another brick or z = 0
             if brick[0][2] == 1 or brick[1][2] == 1:
                 break
             am_blocked, blockers = is_blocked(brick, previous_bricks)
             if am_blocked:
-                h = hashable_brick(brick)
+                h = hashed(brick)
                 if h not in blocking_bricks:
                     blocking_bricks[h] = set()
                 for blocker in blockers:
-                    blocking_bricks[h].add(hashable_brick(blocker))
+                    blocking_bricks[h].add(hashed(blocker))
                 break
             brick[0][2] -= 1
             brick[1][2] -= 1
     return bricks, blocking_bricks
 
 
-def hashable_brick(brick):
+# returns a hashed brick
+def hashed(brick):
     return (
         brick[0][0],
         brick[0][1],
@@ -107,28 +94,32 @@ def hashable_brick(brick):
 
 def part1(input: str) -> int:
     bricks = parse_input(input)
-    pprint.pprint(bricks)
-
-    # sorted by z value. they fall in this order
-    bricks.sort(key=lambda b: min(b[0][2], b[1][2]))
-    pprint.pprint(bricks)
-
-    dropped_bricks, blocking_bricks = drop(bricks)
-    pprint.pprint(dropped_bricks)
-    pprint.pprint(blocking_bricks)
+    _, blocking_bricks = drop(bricks)
 
     solo_blockers = set()
-    for brick, blockers in blocking_bricks.items():
+    for blockers in blocking_bricks.values():
         if len(blockers) == 1:
             solo_blockers.add(list(blockers)[0])
 
     return len(bricks) - len(solo_blockers)
 
 
-def part2(input: str) -> int:
-    raise Exception("Not implemented yet")
-    data = parse_input(input)
-    _ = data
-    # print(data)
+def num_dependers(bricks, dropped_brick):
+    dropped = {hashed(dropped_brick)}
+    while True:
+        no_change = True
+        for k, v in bricks.items():
+            if k in dropped:
+                continue
+            if v.issubset(dropped):
+                dropped.add(k)
+                no_change = False
+        if no_change:
+            break
+    return len(dropped) - 1
 
-    return 0
+
+def part2(input: str) -> int:
+    bricks = parse_input(input)
+    dropped_bricks, blocking_bricks = drop(bricks)
+    return sum([num_dependers(blocking_bricks, b) for b in dropped_bricks])
